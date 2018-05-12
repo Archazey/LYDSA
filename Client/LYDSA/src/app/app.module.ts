fb3c3f489bb2839767debcb5b99003617e5a179c
Baltatu Andrei <baltatu_andrei@yahoo.com>
2018-05-13 00:35:06 +0300

    Added clean angular app, ngx-bootstrap and updated readme

---
+++ Client/LYDSA/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+// ngx-boostrap
+import { AlertModule } from 'ngx-bootstrap';
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    AlertModule.forRoot(),
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 **LYDSA** stands for **_Learn-Your-Data-Structures-and-Algorithms_**. 
 
 It's a web application for vizualizing and simulating basic algorithms (_merge sort, connected components, centroid decomposition ...etc_) and data structures (_queue, stack, segment tree, binary indexed tree ...etc_)
+
+Front-end is done in Angular 5 and ngx-bootstrap ( _https://valor-software.com/ngx-bootstrap/#/_ )
