34efce58de54df1655e9729a25e390f6abcc6134
Baltatu Andrei <baltatu_andrei@yahoo.com>
2018-06-15 01:10:25 +0300

    Added mock problem rating

--- Client/LYDSA/src/app/shared/components/tree-view/tree-view.component.ts
+++ Client/LYDSA/src/app/shared/components/tree-view/tree-view.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 // models
 import { TreeElement } from '../../models/tree-element';
- 
+
 @Component({
   selector: 'app-tree-view',
   templateUrl: './tree-view.component.html',
@@ -10,12 +10,34 @@ import { TreeElement } from '../../models/tree-element';
 })
 export class TreeViewComponent implements OnInit {
   @Input() data: TreeElement[];
-  
-  constructor() { 
+  itemStars: boolean[][] = [];
+  numberOfStars: number = 5;
+  avgRating: number[] = [];
 
+  constructor() {
   }
 
   ngOnInit() {
+    for (var item of this.data) {
+      var starArray = new Array(this.numberOfStars);
+      var stars = this.getRandomInt(0, this.numberOfStars);
+      for (var i = 0; i < this.numberOfStars; i++) {
+        starArray[i] = false;
+        if (i < stars)
+          starArray[i] = true;  
+      }
+      starArray.reverse();
+      this.itemStars.push(starArray);
+      this.avgRating.push(this.getRandom(1, this.numberOfStars).toFixed(2));
+    }
+  }
+
+  getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  getRandom(min, max) {
+    return Math.random() * (max - min) + min;
   }
 
   public toggleItem(item: TreeElement): void {
